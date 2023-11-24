06dd2d54ca3d4b06a5e678cef34143c79653dcc6
Anse98 <lorenzo.anselmi98.com>
2023-11-24 15:33:07 +0100

    ADD-Struttura super base dei file

---
+++ app.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            title: 'To Do List!',
+        }
+    },
+    methods: {
+
+    }
+}).mount('#app')
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link rel="stylesheet" href="./style.css">
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+</head>
+
+<body>
+    <div id="app">
+        <main>
+            <section>
+                <div class="container">
+                    <h1>{{ title }}</h1>
+                    <input type="text">
+                </div>
+
+            </section>
+
+            <section>
+                <div class="container">
+                    <ul>
+                        <li>c</li>
+                        <li>c</li>
+                        <li>c</li>
+                        <li>c</li>
+                    </ul>
+                </div>
+            </section>
+        </main>
+    </div>
+
+    <script src="./app.js"></script>
+</body>
+
+</html>
